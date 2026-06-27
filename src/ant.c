08ea2c2ba586109fc97c07163377033e86b37cb9
obsidian14 <nonewu24@outlook.com>
2026-06-27 16:44:54 +0800

    hello world

---
+++ src/ant.c
@@ -0,0 +1,11 @@
+#include "ant.h"
+#include <stdio.h>
+
+int main(void) {
+  helloWorld();
+  return 0;
+}
+
+void helloWorld(void) {
+  printf("%s\n", "Hello World!");
+}
---
+++ src/ant.h
@@ -0,0 +1,12 @@
+#ifndef ANT_H
+#define ANT_H
+
+#ifdef __cplusplus
+#define ANT_LINKAGE extern "C"
+#else
+#define ANT_LINKAGE
+#endif
+
+ANT_LINKAGE void helloWorld(void);
+
+#endif
